fdcdb34a81ad972bfd0be7bf265a9ea395171d00
Gabriel Silva Lima <gabriel.osilvalima@gmail.com>
2021-06-17 15:33:23 -0300

    Autowired

--- src/main/java/service/AtivaçãoCliente.java
+++ src/main/java/service/AtivaçãoCliente.java
@@ -1,19 +1,30 @@
 package service;
 
 import modelo.Cliente;
+import notificacao.Notificador;
 import notificacao.NotificadorEmail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AtivaçãoCliente {
     private NotificadorEmail notificador;
 
-    public AtivaçãoCliente(NotificadorEmail notificador) {
-        this.notificador = notificador;
-    }
+    //@Autowired
+    // public AtivaçãoCliente(NotificadorEmail notificador) {
+    //
+    //     this.notificador = notificador;
+    // }
+    // public AtivaçãoCliente(String qualquer){
+    //
+    //   }
 
     public void ativar(Cliente cliente){
         cliente.ativar();
         notificador.notificador(cliente, "Seu cadastro no sistema esta ativo");
-
-
+    }
+    @Autowired
+    public void setNotificador(NotificadorEmail notificador) {
+        this.notificador = notificador;
     }
 }

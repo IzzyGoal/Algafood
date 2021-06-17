17359849cb69dd6b96d25fef82a26af0af63438c
Gabriel Silva Lima <gabriel.osilvalima@gmail.com>
2021-06-17 09:12:46 -0300

    Classe NotificadorConfig

--- src/main/java/com/algaworks/algafoods/NotificacaoConfig.java
+++ src/main/java/com/algaworks/algafoods/NotificacaoConfig.java
@@ -3,20 +3,17 @@ package com.algaworks.algafoods;
 import notificacao.NotificadorEmail;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import service.AtivaçãoCliente;
 
 @Configuration
-public class AlgaConfig {
+public class NotificacaoConfig {
 
     @Bean
     public NotificadorEmail notificadorEmail(){
         NotificadorEmail notificador = new NotificadorEmail("smtp.algamail.com.br");
         notificador.setCaixaAlta(true);
+
         return notificador;
     }
-    @Bean
-    public AtivaçãoCliente ativaçãoCliente(){
-        return new AtivaçãoCliente( notificadorEmail());
-    }
+
 
 }

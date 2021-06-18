50d2dec8e739c146c230da6f8f14a94cfe2c9981
Gabriel Silva Lima <gabriel.osilvalima@gmail.com>
2021-06-18 14:09:45 -0300

    Classe Mockada

--- src/main/java/notificacao/NotificadorEmailMock.java
+++ src/main/java/notificacao/NotificadorEmailMock.java
@@ -1,18 +1,17 @@
 package notificacao;
 
 import modelo.Cliente;
-
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-@Profile("prod")
+@Profile("")
 @NivelDeUrgencia(value = NivelUrgencia.URGENTE)
 @Component
-public class NotificadorEmail implements Notificador {
+public class NotificadorEmailMock implements Notificador {
 
     @Override
     public void notificador(Cliente cliente, String menssagem) {
-        System.out.printf("Notificando %s atraves do email %s: %s\n",
+        System.out.printf("MOCK: Notificando %s atraves do email %s: %s\n",
                 cliente.getNome(),
                 cliente.getEmail(),
                 menssagem);
